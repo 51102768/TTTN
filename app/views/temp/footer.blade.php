06dca67b85227913538462e29399ca25ee06870c
51102768 <nt.quan1704@gmail.com>
2015-04-22 23:08:27 +0700

    change

--- app/views/temp/footer.blade.php
+++ app/views/temp/footer.blade.php
@@ -1,28 +1,29 @@
 <footer>
       <div class = "footer">
         <div class = "container-fluid">
-        	<div class="row">
-				<div  class="col col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        	<div class="row" style="background-color: #CCCC99">
+				<div  class="col col-xs-12 col-sm-12 col-md-12 col-lg-12" >
 					<div class = "social-cont">
 						<div class = "social">
-						  <a class = "social-item">
+						  <a href="http://www.facebook.com" class = "social-item">
 						    <img src="img/fbicon.png" width="50%" height="50%" class="img-responsive" alt="Responsive image"/>
 						  </a>
-						  <a class = "social-item">
+						  <a href="http://www.twitter.com" class = "social-item">
 						    <img src="img/twicon.png" width="50%" height="50%" class="img-responsive" alt="Responsive image"/>
 						  </a>
-						  <a class = "social-item">
+						  <a href= "http://www.skype.com" class = "social-item">
 						    <img src="img/skype.png"  width="50%" height="50%" class="img-responsive" alt="Responsive image"/>
 						  </a>
 						   </ul>        
-						</div>    
+						</div>
+						<h2>Mỡ Shop</h2>
+
 					</div>
 				</div>
 			</div>
           <div class = "row">
             <div class = "col-md-6">
-              <h1>Mỡ Shop</h3>
-            </div>
+                          </div>
             <div class = "col-md-6">
               <h3>Khách hàng là thượng đế</h3>
             </div>

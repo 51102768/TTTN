34626514b76501c4c5f9734e5775c6cd9193cee0
Ken Lâm <hoangvu23108@gmail.com>
2015-05-17 16:51:50 +0700

    Insert slider

--- app/views/homepage.blade.php
+++ app/views/homepage.blade.php
@@ -1,4 +1,23 @@
 @extends('master')
+
+@section('header')
+	<link
+		type="text/css"
+		rel="stylesheet"
+		href="{{ asset("css/content.css") }}"
+		/>
+	<script type="text/javascript" src="{{ asset("js/jquery.jcarousel.pack.js")}}"></script>
+@stop
+
 @section('content')
-asdasd
+	<div class="slider">
+		<div class="slider-holder">
+			<img src="img/slider/apple.jpg">
+		</div>	
+		<div class="slider-nav">
+		</div>
+	</div>
+	<div class="product"></div>
+	
+
 @stop
